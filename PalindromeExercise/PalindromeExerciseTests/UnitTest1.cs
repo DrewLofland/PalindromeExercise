cb6ee01bcf0c2d357cd2acd39c970bc393da65d5
Drew Lofland <alofland25@gmail.com>
2023-01-23 17:58:32 -0500

    exercise complete

--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,3 +1,4 @@
+using PalindromeExercise;
 using System;
 using Xunit;
 
@@ -5,10 +6,18 @@ namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
-        {
+        [Theory]
+        [InlineData("racecar", true)]
+        [InlineData("hello", false)]
+        [InlineData("kayak", true)]
+        [InlineData("palindrome", false)]
+        [InlineData("madam", true)]
 
+        public void PalindromeTest(string word, bool expected)
+        {
+            var test = new WordSmith();
+            var actual = test.IsAPalindrome(word);
+            Assert.Equal(expected, actual);
         }
     }
 }
